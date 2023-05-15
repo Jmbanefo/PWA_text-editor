c92ad7d804f0c5ce8d2fa81e8d6167b83d9a9ca1
Justin <mbanefojustin@gmail.com>
2023-05-15 14:07:09 -0500

    updated develope

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,17 +13,17 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.log('putDb to the database');
+export const putDb = async (content) => {console.log('putDb to the database');
   const myDb = await initdb(); 
   const tx = myDb.transaction('jate', 'readwrite'); 
   const stored = tx.objectStore('jate'); 
   await stored.put({ value: content, id: 1}); 
   await tx.done; 
   console.log("Successfully added to the database");
-
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('Get content from database');
+export const getDb = async () => {console.error('Get content from database');
   const teDb = await initdb(); 
   const tx = teDb.transaction('jate', 'readonly'); 
   const stash = tx.objectStore('jate'); 
@@ -31,6 +31,6 @@ export const getDb = async () => console.error('Get content from database');
   await tx.done; 
   console.log("Displayed is all the content from the databade", stash);
   return get; 
-
+}
 
 initdb();
